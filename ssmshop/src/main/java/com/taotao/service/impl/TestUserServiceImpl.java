fdb13fed3d0ffa5160b1d67a2236345c518176f0
gengxiong <lancegeng@outlook.com>
2018-08-13 09:20:39 +0800

    完成页面跳转

--- ssmshop/src/main/java/com/taotao/service/impl/TestUserServiceImpl.java
+++ ssmshop/src/main/java/com/taotao/service/impl/TestUserServiceImpl.java
@@ -2,6 +2,7 @@ package com.taotao.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.taotao.mapper.TbItemMapper;
 import com.taotao.mapper.TbUserMapper;
 import com.taotao.pojo.TbUser;
 import com.taotao.service.TestUserService;
